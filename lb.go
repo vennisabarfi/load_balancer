fe03676af3de84d01a2a62343374c8065e4aa6eb
unknown <vennisabarfi@gmail.com>
2025-02-01 02:07:19 -0500

    able to extract ip address from client now. off to response message

--- lb.go
+++ lb.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"net/netip"
 )
+
+type Response struct {
+	IPAddress netip.Addr //store ip addresses
+	Method    string     //request method eg GET
+	Host      string
+}
+
+func getIPAddress(r *http.Request) string {
+	// retrive ip address from request
+	ip := r.RemoteAddr
+	parsed_ip, _, err := net.SplitHostPort(ip)
+	if err != nil {
+		log.Print("Error parsing ip address of client")
+	}
+	return parsed_ip
+}
 
 // main server for load balancer
 func MainServer(w http.ResponseWriter, r *http.Request) {
+	// extract client ip address from parsed_ip
+	client := getIPAddress(r)
+	fmt.Print(client)
 	fmt.Println("Hello World")
+
 }
