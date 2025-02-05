40ca7c746aba9088ab3522be4bbb0614b42e8b1c
unknown <vennisabarfi@gmail.com>
2025-02-04 23:01:44 -0500

    successfully forwarded request from the backend server. learnt a lot. now off to step two and implement the scheduling algo which is rhe round robin one

--- be.go
+++ be.go
@@ -10,6 +10,9 @@ import (
 func BackendServer(w http.ResponseWriter, r *http.Request) {
 	// loadresponse := LoadBalancer(w, r)
 	// io.WriteString(os.Stdout, response)
-	fmt.Println("Hello World")
+	backendResponse := "Hello From Backend Server"
+
+	fmt.Println("Hello From Backend Server")
+	w.Write([]byte(backendResponse))
 
 }
--- lb.go
+++ lb.go
@@ -40,20 +40,45 @@ func getIPAddress(r *http.Request) string {
 // main server for load balancer
 func LoadBalancer(w http.ResponseWriter, r *http.Request) {
 	// extract client ip address from parsed_ip
-	client := getIPAddress(r)
+	clients := getIPAddress(r)
 	host := strings.Split(r.Host, ":")[0]
 	user_agent := r.UserAgent()
 
-	response := fmt.Sprintf("Received request from %s. \n%s %s \n Host: %s \n User-Agent: %s", client, r.Method, r.Proto, host, user_agent)
+	response := fmt.Sprintf("Received request from %s. \n%s %s \n Host: %s \n User-Agent: %s", clients, r.Method, r.Proto, host, user_agent)
 	// w.WriteHeader(http.StatusOK)
 
-	// create a new request to the backend server
-	serverPort := "3000"
-	requestURL := fmt.Sprintf("http://localhost:%d", serverPort)
-	fmt.Print(requestURL)
+	// forward request made to load balancer to backend server
+
+	// create a new request/connection to the backend server
+	// serverPort := "3000"
+	// requestURL := fmt.Sprintf("http://localhost:%d", serverPort)
+	// fmt.Print(requestURL)
+
+	// change this to newrequest with context
+	req, err := http.NewRequest("GET", "http://localhost:3000", nil) //method, url and request body
+	if err != nil {
+		log.Println("Error reaching backend server", err)
+	}
+	// set user-agent as header
+	req.Header.Set("User-Agent", user_agent)
+
+	client := &http.Client{}
+	req.Header.Add("If-None-Match", `W/"wyzzy"`)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error sending http request to the backend server", err)
+	}
+	defer resp.Body.Close()
+	// backend server response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error reading backend server body", err)
+	}
+	w.Write(body)
+	io.WriteString(os.Stdout, string(body))
 	// print to server and also write response
 	io.WriteString(os.Stdout, response)
-	w.Write([]byte(response))
+	// w.Write([]byte(response))
 	// io.WriteString(w, response)
 
 }
